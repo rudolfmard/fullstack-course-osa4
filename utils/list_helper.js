a6eb5f326794aeefb972bd836e1ffc9d2ce1ac41
rudolf <rudolf.mard@gmail.com>
2023-02-15 17:14:43 +0200

    Tehtävä 4.4: apufunktioita ja yksikkötestejä step 2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,18 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    if (blogs.length === 0){
+        return 0
+    }
+    let sum = 0
+    blogs.forEach(element => {
+        sum += element.likes
+    })
+    return sum
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
