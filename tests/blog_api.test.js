5001b9a30dd1eaf03850bf9bf2e62904b335d432
rudolf <rudolf.mard@gmail.com>
2023-02-16 19:05:37 +0200

    Tehtävä 4.10: blogilistan testit step3

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -29,3 +29,20 @@ test('Blog object contains field "id"', async () => {
         expect(blog.id).toBeDefined()
     }
 })
+
+test('After adding one blog the number of blogs increment by one', async () => {
+    const testBlog = {
+        title: "Test blog",
+        author: "Testi Jannu",
+        url: "https://eioo.com/",
+        likes: 0
+    }
+    await api
+        .post('/api/blogs')
+        .send(testBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+    
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+})
