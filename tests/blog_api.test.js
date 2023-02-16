ab67d2aae8083110641b73b4d078a4610cb33903
rudolf <rudolf.mard@gmail.com>
2023-02-16 17:30:50 +0200

    Tehtävä 4.8: blogilistan testit step1

---
+++ tests/blog_api.test.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
+})
+
+test('The blogs are returned as JSON', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
+
+test('The correct number of blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
