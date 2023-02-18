cd24c42b0ce87212a7fc0de9114a89fea5caded4
rudolf <rudolf.mard@gmail.com>
2023-02-18 16:56:03 +0200

    Tehtävä 4.22*: blogilistan laajennus step10

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,44 +2,33 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
+const { userExtractor } = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
 	const blogs = await Blog.find({}).populate('user', {username:1, name:1, id:1})
 	response.json(blogs)
 })
   
-blogsRouter.post('/', async (request, response) => {
-	const decodedToken = jwt.verify(request.token, process.env.SECRET)
-	if (!decodedToken.id){
-		return response.status(401).json({error: 'invalid token'})
-	}
-	const user = await User.findById(decodedToken.id)
-
+blogsRouter.post('/', userExtractor, async (request, response) => {
 	const body = request.body
     const blog = new Blog({
 		title: body.title,
 		author: body.author,
 		url: body.url,
 		likes: body.likes === undefined ? 0 : body.likes,
-		user: user._id
+		user: request.user._id
 	})
 	const savedBlog = await blog.save()
 
-	user.blogs = user.blogs.concat(savedBlog._id)
-	await user.save()
+	request.user.blogs = request.user.blogs.concat(savedBlog._id)
+	await request.user.save()
 
 	response.status(201).json(savedBlog)
 })
 
-blogsRouter.delete('/:id', async (request, response) => {
-	const decodedToken = jwt.verify(request.token, process.env.SECRET)
-	if (!decodedToken.id){
-		return response.status(401).json({error: 'invalid token'})
-	}
-	const user = await User.findById(decodedToken.id)
-
+blogsRouter.delete('/:id', userExtractor, async (request, response) => {
 	const blog = await Blog.findById(request.params.id)
-	if (blog.user.toString() !== user._id.toString()){
+	if (blog.user.toString() !== request.user._id.toString()){
 		return response.status(401).json({error: 'Token belongs to unauthorized user'})
 	}
 
